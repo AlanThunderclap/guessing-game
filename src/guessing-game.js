11632a6e86db6119b4dca987a9397b3899ec9c17
Aleksei Lazarenko <alan.thunderclap@mail.ru>
2019-10-09 15:50:07 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.last = Math.round((this.left + this.right) / 2);
+        return this.last;
     }
 
     lower() {
-
+        this.right = this.last;
     }
 
     greater() {
-
+        this.left = this.last;
     }
 }
 
